164bb6da36a1db823e4c825a51281ee88fb8301d
goreleaserbot <goreleaser@carlosbecker.com>
2021-05-17 10:58:31 +0300

    Brew formula update for homebew-mytun version v1.3.16

--- Formula/mytun.rb
+++ Formula/mytun.rb
@@ -5,25 +5,25 @@
 class Mytun < Formula
   desc "MyTun.net - Expose any Port securely on internet"
   homepage "https://dashboard.mytun.net/"
-  version "1.3.15"
+  version "1.3.16"
   license "BSD 3-Clause License"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/mytunnet/homebew-mytun/releases/download/v1.3.15/mytun_1.3.15_darwin_amd64.tar.gz"
-    sha256 "cb315c1a872bb9074c8f357a94d7f1791bc101596be4a007a4a55d09a0b93c65"
+    url "https://github.com/mytunnet/homebew-mytun/releases/download/v1.3.16/mytun_1.3.16_darwin_amd64.tar.gz"
+    sha256 "276b5b6adcc1803f86897f1a67661cc31af385d526f9af64dd74ec106be0e823"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/mytunnet/homebew-mytun/releases/download/v1.3.15/mytun_1.3.15_darwin_arm64.tar.gz"
-    sha256 "34ae15a92e05ad090fc89d65a990f2ff9f2f50698ebe8b863d052c0fb03ea941"
+    url "https://github.com/mytunnet/homebew-mytun/releases/download/v1.3.16/mytun_1.3.16_darwin_arm64.tar.gz"
+    sha256 "864f5d02df9706896219b78d6267bc409f3bc28727cb05331a93bd43271b68cb"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/mytunnet/homebew-mytun/releases/download/v1.3.15/mytun_1.3.15_linux_amd64.tar.gz"
-    sha256 "7df6ce5368c3af7a18558ede639e43bb640c818520484d7c2861b076b9629fe5"
+    url "https://github.com/mytunnet/homebew-mytun/releases/download/v1.3.16/mytun_1.3.16_linux_amd64.tar.gz"
+    sha256 "dd64e981b799c9072669a84fd908e171547cebf6f46c765fc6d69575cae9d80a"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/mytunnet/homebew-mytun/releases/download/v1.3.15/mytun_1.3.15_linux_arm64.tar.gz"
-    sha256 "5aa6cf12b19964b6837308c240564451479b723f9cb0e1c24f734a0d711be5af"
+    url "https://github.com/mytunnet/homebew-mytun/releases/download/v1.3.16/mytun_1.3.16_linux_arm64.tar.gz"
+    sha256 "39bf96a472b60c240c038725e5cb4a0ade700b5833a38be88d41bf1aaf71d648"
   end
 
   def install
@@ -31,7 +31,7 @@ class Mytun < Formula
   end
 
   def caveats; <<~EOS
-    How to use this binary
+    Usage: mytun YOUR_DEVICE_TOKEN
   EOS
   end
 end
