384cafddd70f2f97ca8c60cd2a0416492bff58e9
goreleaserbot <goreleaser@carlosbecker.com>
2021-05-17 10:47:26 +0300

    Brew formula update for homebew-mytun version v1.3.15

--- Formula/mytun.rb
+++ Formula/mytun.rb
@@ -5,29 +5,29 @@
 class Mytun < Formula
   desc "MyTun.net - Expose any Port securely on internet"
   homepage "https://dashboard.mytun.net/"
-  version "1.3.14"
+  version "1.3.15"
   license "BSD 3-Clause License"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/mytunnet/homebew-mytun/releases/download/v1.3.14/mytun_1.3.14_darwin_amd64.tar.gz"
-    sha256 "51e0584a51c86492af112070a221da37be8cb5788238a2e54e9add336c662db1"
+    url "https://github.com/mytunnet/homebew-mytun/releases/download/v1.3.15/mytun_1.3.15_darwin_amd64.tar.gz"
+    sha256 "cb315c1a872bb9074c8f357a94d7f1791bc101596be4a007a4a55d09a0b93c65"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/mytunnet/homebew-mytun/releases/download/v1.3.14/mytun_1.3.14_darwin_arm64.tar.gz"
-    sha256 "d76036ec52b8d621958301dcec36fc27c8280be9a71639f5ff8eab51f77f8486"
+    url "https://github.com/mytunnet/homebew-mytun/releases/download/v1.3.15/mytun_1.3.15_darwin_arm64.tar.gz"
+    sha256 "34ae15a92e05ad090fc89d65a990f2ff9f2f50698ebe8b863d052c0fb03ea941"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/mytunnet/homebew-mytun/releases/download/v1.3.14/mytun_1.3.14_linux_amd64.tar.gz"
-    sha256 "bf0ba5bde2243d557ff8fdf3eb2b14c10f69f33286499163ffa7bbea1330e15b"
+    url "https://github.com/mytunnet/homebew-mytun/releases/download/v1.3.15/mytun_1.3.15_linux_amd64.tar.gz"
+    sha256 "7df6ce5368c3af7a18558ede639e43bb640c818520484d7c2861b076b9629fe5"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/mytunnet/homebew-mytun/releases/download/v1.3.14/mytun_1.3.14_linux_arm64.tar.gz"
-    sha256 "4606f5b80fa7f19cc050cec01813b24bd2d2cd0d6ba3a9091503dd0539d90fd5"
+    url "https://github.com/mytunnet/homebew-mytun/releases/download/v1.3.15/mytun_1.3.15_linux_arm64.tar.gz"
+    sha256 "5aa6cf12b19964b6837308c240564451479b723f9cb0e1c24f734a0d711be5af"
   end
 
   def install
-    bin.install "homebew-mytun"
+    bin.install "mytun"
   end
 
   def caveats; <<~EOS
